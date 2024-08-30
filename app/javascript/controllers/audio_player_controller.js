22f2578290b3f6d208b39f90c976f43aa6c4127e
AmiasYaska <amiasyaska@gmail.com>
2024-08-30 18:52:28 +0300

    changes

--- app/javascript/controllers/audio_player_controller.js
+++ app/javascript/controllers/audio_player_controller.js
@@ -4,9 +4,10 @@ import WaveSurfer from 'wavesurfer.js'
 // Connects to data-controller="audio-player"
 export default class extends Controller {
   static values = { url: String};
+  static targets = ["preview"]
   connect() {
     this.wavesurfer = WaveSurfer.create({
-      container: this.element,
+      container: this.previewTarget,
       waveColor: '#4F4A85',
       progressColor: '#383351',
       url: this.urlValue,

9cbacf19da569f7779f0539a3b79aaf3c63d3532
AmiasYaska <amiasyaska@gmail.com>
2024-08-30 17:22:11 +0300

    changes

--- app/javascript/controllers/audio_player_controller.js
+++ app/javascript/controllers/audio_player_controller.js
@@ -1,7 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
+import WaveSurfer from 'wavesurfer.js'
 
 // Connects to data-controller="audio-player"
 export default class extends Controller {
   connect() {
+    this.wavesurfer = WaveSurfer.create({
+      container: this.element,
+      waveColor: '#4F4A85',
+      progressColor: '#383351',
+      url: this.urlValue,
+    })
   }
 }
